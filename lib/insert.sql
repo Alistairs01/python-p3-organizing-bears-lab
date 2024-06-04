23a7ff7504670cf1a1d3f08f4aa9f56566740ced
Alistairs <alistairs.gatheru@student.moringaschool.com>
2024-06-04 11:26:09 +0300

    organising baers

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 10, 'M', 'brown', 'calm', 1),
+('Rowdy', 7, 'M', 'black', 'aggressive', 1),
+('Tabitha', 3, 'F', 'blonde', 'friendly', 1),
+('Sergeant Brown', 5, 'M', 'brown', 'serious', 1),
+('Melissa', 4, 'F', 'black', 'playful', 1),
+('Grinch', 6, 'M', 'white', 'grumpy', 1),
+('Wendy', 2, 'F', 'brown', 'curious', 1),
+(NULL, 0, NULL, NULL, NULL, 0);
